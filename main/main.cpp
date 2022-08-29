80b8ee06fcc4ecef69cbe58147ac40a2c6e48595
庞碧璋 <11040312+pang-bizhang@user.noreply.gitee.com>
2022-08-30 00:00:07 +0800

    lvgl learn

--- main/main.cpp
+++ main/main.cpp
@@ -1,27 +1,20 @@
-#include "oled.h"
-#include "freertos\FreeRTOS.h"
-#include "freertos\task.h"
-
 #include <stdio.h>
 
-extern Ssd1306_hal_handle_t oled_spi4_fun_t;
+#include "lvgl.h"
 
 extern "C" void app_main(void){
-    auto oled = Oled::getInstance();
-    uint8_t x = 0;
-    oled->autoRefresh(200);
-    oled->drawLine(127,0,63,31);
-    oled->drawLine(0,0,63,31);
-    while(1){
-        oled->clear();
-        oled->drawLine(x,1,63,32);
-        oled->drawLine(128 - x,1,63,32);
-        x++;
-        if(x > 128){
-            x = 0;
-        }
-        vTaskDelay( 20 );
-    }
+    lv_disp_drv_t* disp_driver = NULL;
+    disp_driver = new lv_disp_drv_t;
 
+    lv_init();
+
+    lv_disp_drv_init(disp_driver);
+    
+    lv_disp_drv_register(disp_driver);
+
+    while(1){
+        lv_tick_inc(1);
+        lv_timer_handler();
+    }
 }
 
