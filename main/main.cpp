aabfc4ce3b328f2734f6c959d7a60dedcd8cf3dd
庞碧璋 <11040312+pang-bizhang@user.noreply.gitee.com>
2022-08-30 21:29:25 +0800

    lvgl CMacke改动 移植

--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 #include "lvgl.h"
+#include "demos/lv_demos.h"
 
 extern "C" void app_main(void){
     lv_disp_drv_t* disp_driver = NULL;
     disp_driver = new lv_disp_drv_t;
 
     lv_init();
-
     lv_disp_drv_init(disp_driver);
-    
     lv_disp_drv_register(disp_driver);
+
+    lv_demo_widgets();
 
     while(1){
         lv_tick_inc(1);
